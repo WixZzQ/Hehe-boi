7ce413259462231e51b9aa2e5eb91787dc81c6e0
unknown <dangoshuprappo@gmail.com>
2025-01-28 18:14:59 +0600

    done

--- tailwindcssconfig.js
+++ tailwindcssconfig.js
@@ -1,9 +1,18 @@
-tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
-    }
+// tailwind.config = {
+//     theme: {
+//       extend: {
+//         colors: {
+//           clifford: '#da373d',
+//         }
+//       }
+//     }
+//   }
+
+let num = 0;
+setInterval(() => {
+  num++;
+  console.log(num)
+  if (num === 10) {
+    clearInterval(interval);
   }
+}, 2000)
